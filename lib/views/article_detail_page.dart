58df8e60e0a5bf84a977f75decefed0a5199ab19
Gary Lin 林品辰 <garylin2018@gmail.com>
2024-03-02 05:20:53 +0800

    feat: create article detail page

---
+++ lib/views/article_detail_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:bullet_news/models/news.dart';
+
+class ArticleDetailPage extends StatelessWidget {
+  final News news;
+
+  const ArticleDetailPage({super.key, required this.news});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(news.title),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(news.urlToImage),
+            const SizedBox(height: 8),
+            Text(
+              news.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              news.publishedAt,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              news.summary,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
