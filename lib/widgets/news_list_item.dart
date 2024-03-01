9beef00d124d0925fdf117d2ae6314ed642cc6d9
Gary Lin 林品辰 <garylin2018@gmail.com>
2024-03-02 05:21:17 +0800

    feat: add inkwell and navigation to articlle detail page

--- lib/widgets/news_list_item.dart
+++ lib/widgets/news_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:bullet_news/models/news.dart';
+import 'package:bullet_news/views/article_detail_page.dart';
 import 'package:flutter/material.dart';
 
 class NewsListItem extends StatelessWidget {
@@ -7,40 +8,50 @@ class NewsListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(8.0),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (news.urlToImage.isNotEmpty)
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: Image.network(
-                  news.urlToImage,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                  height: 200,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ArticleDetailPage(news: news),
+          ),
+        );
+      },
+      child: Card(
+        margin: const EdgeInsets.all(8.0),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (news.urlToImage.isNotEmpty)
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.network(
+                    news.urlToImage,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                    height: 200,
+                  ),
                 ),
+              const SizedBox(height: 8),
+              Text(
+                news.title,
+                style: Theme.of(context).textTheme.titleLarge,
+                softWrap: true,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
               ),
-            const SizedBox(height: 8),
-            Text(
-              news.title,
-              style: Theme.of(context).textTheme.titleLarge,
-              softWrap: true,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 2,
-            ),
-            const SizedBox(height: 8),
-            Text(
-              news.description,
-              style: Theme.of(context).textTheme.bodyMedium,
-              softWrap: true,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 3,
-            ),
-          ],
+              const SizedBox(height: 8),
+              Text(
+                news.description,
+                style: Theme.of(context).textTheme.bodyMedium,
+                softWrap: true,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 3,
+              ),
+            ],
+          ),
         ),
       ),
     );
