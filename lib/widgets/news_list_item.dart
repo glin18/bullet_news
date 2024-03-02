d0a80adc50952aecf64cc6d01a9eff964dd188b5
Gary Lin 林品辰 <garylin2018@gmail.com>
2024-03-02 16:33:43 +0800

    refactor: refine news list item for cleaner code and readability

--- lib/widgets/news_list_item.dart
+++ lib/widgets/news_list_item.dart
@@ -9,14 +9,7 @@ class NewsListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ArticleDetailPage(news: news),
-          ),
-        );
-      },
+      onTap: () => _navigateToArticleDetailPage(context),
       child: Card(
         margin: const EdgeInsets.all(8.0),
         child: Padding(
@@ -24,57 +17,69 @@ class NewsListItem extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: news.urlToImage.isNotEmpty
-                    ? Image.network(
-                        news.urlToImage,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                        height: 200,
-                        errorBuilder: (context, error, stackTrace) {
-                          debugPrint(news.urlToImage);
-                          return Container(
-                            width: double.infinity,
-                            height:
-                                200, // Specify a fixed height for consistency
-                            decoration: BoxDecoration(
-                              color: const Color.fromARGB(
-                                  255, 57, 52, 58), // A light grey background
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          );
-                        },
-                      )
-                    : Container(
-                        width: double.infinity,
-                        height: 200, // Specify a fixed height for consistency
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(
-                              255, 57, 52, 58), // A light grey background
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-              ),
+              _buildImage(context),
               const SizedBox(height: 8),
-              Text(
-                news.title,
-                style: Theme.of(context).textTheme.titleLarge,
-                softWrap: true,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-              ),
+              _buildTitle(context),
               const SizedBox(height: 8),
-              Text(
-                news.description,
-                style: Theme.of(context).textTheme.bodyMedium,
-                softWrap: true,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 3,
-              ),
+              _buildDescription(context),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  void _navigateToArticleDetailPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => ArticleDetailPage(news: news),
+      ),
+    );
+  }
+
+  Widget _buildTitle(BuildContext context) {
+    return Text(
+      news.title,
+      style: Theme.of(context).textTheme.titleLarge,
+      softWrap: true,
+      overflow: TextOverflow.ellipsis,
+      maxLines: 2,
+    );
+  }
+
+  Widget _buildDescription(BuildContext context) {
+    return Text(
+      news.description,
+      style: Theme.of(context).textTheme.bodyMedium,
+      softWrap: true,
+      overflow: TextOverflow.ellipsis,
+      maxLines: 3,
+    );
+  }
+
+  Widget _buildImage(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8.0),
+      child: news.urlToImage.isNotEmpty
+          ? Image.network(
+              news.urlToImage,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              height: 200,
+              errorBuilder: (_, __, ___) => _imageFallback(),
+            )
+          : _imageFallback(),
+    );
+  }
+
+  Widget _imageFallback() {
+    return Container(
+      width: double.infinity,
+      height: 200,
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 57, 52, 58),
+        borderRadius: BorderRadius.circular(8),
       ),
     );
   }
