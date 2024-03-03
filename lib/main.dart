7824e505d112aed880abf7148e51ba2cc58d082b
Gary Lin 林品辰 <garylin2018@gmail.com>
2024-03-03 17:52:14 +0800

    feat: add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:bullet_news/views/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
