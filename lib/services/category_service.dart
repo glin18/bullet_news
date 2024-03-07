3e283ecaccc44976011760380ac7036657e7b4b7
Gary Lin 林品辰 <garylin2018@gmail.com>
2024-03-07 15:12:07 +0800

    feat: add firebase token to authorization header of requests

--- lib/services/category_service.dart
+++ lib/services/category_service.dart
@@ -1,4 +1,5 @@
 import 'package:bullet_news/models/category.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -6,7 +7,17 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class CategoryService {
   Future<List<Category>> fetchNews() async {
     String baseUrl = dotenv.env['API_BASE_URL'] ?? "";
-    final response = await http.get(Uri.parse('$baseUrl/api/category'));
+    String? token = await FirebaseAuth.instance.currentUser?.getIdToken();
+    if (token == null) {
+      throw Exception('Not authenticated');
+    }
+    final response = await http.get(
+      Uri.parse('$baseUrl/api/category'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
     if (response.statusCode == 200) {
       List jsonResponse = json.decode(response.body);
       return jsonResponse.map((news) => Category.fromJson(news)).toList();
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -2,12 +2,22 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../models/news.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class NewsService {
   Future<List<News>> fetchNews(String id) async {
     String baseUrl = dotenv.env['API_BASE_URL'] ?? "";
-    final response =
-        await http.get(Uri.parse('$baseUrl/api/category/$id/news'));
+    String? token = await FirebaseAuth.instance.currentUser?.getIdToken();
+    if (token == null) {
+      throw Exception('Not authenticated');
+    }
+    final response = await http.get(
+      Uri.parse('$baseUrl/api/category/$id/news'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
     if (response.statusCode == 200) {
       List jsonResponse = json.decode(response.body);
       return jsonResponse.map((news) => News.fromJson(news)).toList();
