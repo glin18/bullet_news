40cba60b728251bc35d95712b48b29472c0ed05a
Gary Lin 林品辰 <garylin2018@gmail.com>
2024-03-12 16:49:42 +0800

    feat: add comment service

---
+++ lib/services/comment_service.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import '../models/comment.dart';
+
+class CommentService {
+  Future<List<Comment>> fetchCommentsForNews(String newsId) async {
+    String baseUrl = dotenv.env['API_BASE_URL'] ?? "";
+    String? token = await FirebaseAuth.instance.currentUser?.getIdToken();
+    if (token == null) {
+      throw Exception('Not authenticated');
+    }
+
+    final response = await http.get(
+      Uri.parse('$baseUrl/api/comments/news/$newsId'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((comment) => Comment.fromJson(comment)).toList();
+    } else {
+      throw Exception('Failed to load news');
+    }
+  }
+
+  Future<Comment> addCommentForNews(String newsId, String content) async {
+    String baseUrl = dotenv.env['API_BASE_URL'] ?? "";
+    String? token = await FirebaseAuth.instance.currentUser?.getIdToken();
+    if (token == null) {
+      throw Exception('Not authenticated');
+    }
+
+    final response = await http.post(
+      Uri.parse('$baseUrl/api/comments/news/$newsId'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'content': content,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonResponse = json.decode(response.body);
+      return Comment.fromJson(jsonResponse);
+      ;
+    } else {
+      throw Exception('Failed to load news');
+    }
+  }
+}
